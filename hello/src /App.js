0843c625348ddcf5baecf2d7b03a7b73c72cf2af
Julian Sandersius <sandersiusjulian@gmail.com>
2018-05-31 00:50:11 -0700

    notes within app.js

--- hello/src /App.js
+++ hello/src /App.js
@@ -1,3 +1,8 @@
+// why are we rendering a component within another component
+// app.js renders hello app component
+//we are using index.js to render compenent to tthe page 
+// app.js will be the single wrapping componet for whole app
+
 import React from "react";
 import HelloReact from "./components/HelloReact";
 
