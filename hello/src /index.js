b9f3a9b73ca1dc1af04743e3a43e3f163af08c84
Julian Sandersius <sandersiusjulian@gmail.com>
2018-05-30 23:55:01 -0700

    further notes in index.js

--- hello/src /index.js
+++ hello/src /index.js
@@ -4,3 +4,4 @@ import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 // this renders the app component which is imported from app.js to the element with id-root
+// this root element can be found in the index.html file, in the public folder 
