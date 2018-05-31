d0a44c54bab5f7a9596291ae81b816aa7e784e22
Julian Sandersius <sandersiusjulian@gmail.com>
2018-05-30 23:51:24 -0700

    notes within index.js

--- hello/src /index.js
+++ hello/src /index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React from "react";   //like require --> read innotes
 import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+// this renders the app component which is imported from app.js to the element with id-root
