d1167ad7c904a5e2ec94985540847e9f9d93bb9f
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-10 13:11:26 -0700

    save

--- solved/src/App.js
+++ solved/src/App.js
@@ -4,3 +4,12 @@ import HelloDiv from "./components/HelloDiv";
 const App = () => <HelloDiv />;
 
 export default App;
+
+
+//app.js renders the hello div component, why render a single component within another coponent?
+//why not just render the compnent that contains content
+//we are learnng to build coponents and their relationshipts
+//we are using the index.js to render the component to the page, and we could have easily built the application 
+	//in app.js 
+//but most of the time app.js will include multiple components, 
+//app.js will be the single wrapping component for the whole app
