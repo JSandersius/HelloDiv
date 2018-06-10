1f2b2fea89f76fcc9a8d3133e978c24b783cea9b
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-10 12:37:02 -0700

    notes edit

--- solved/src/index.js
+++ solved/src/index.js
@@ -4,23 +4,33 @@ import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
+
+
+
+
+
+
+
 //import is jsx syntax, similar to require 
 
+//q
+//import vs require 
 // In a project I'm collaborating on, we have two choices on which module system we can use:
 // Importing modules using require, and exporting using module.exports and exports.foo.
 // Importing modules using ES6 import, and exporting using ES6 export
 // Are there any performance benefits to using one over the other? Is 
-//there anything else that we should know if we were to use ES6 modules over Node ones?
+// there anything else that we should know if we were to use ES6 modules over Node ones?
 
+
+//a
 // There are several usage / capabilities you might want to consider:
 
 // Require:
-
 // You can have dynamic loading where the loaded module name isn't predefined /static, or where you conditionally 
 //load a module only if it's "truly required" (depending on certain code flow).
 // Loading is synchronous. That means if you have multiple requires, they are loaded and processed one by one.
-// ES6 Imports:
 
+// ES6 Imports:
 // You can use named imports to selectively load only the pieces you need. That can save memory.
 // Import can be asynchronous (and in current ES6 Module Loader, it in fact is) and can perform a little better.
 // Also, the Require module system isn't standard based. It's is highly unlikely to become standard now that 
