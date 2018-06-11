eb4e5dd9db026502240e2411d608ebdf9689f7c5
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-11 09:44:01 -0700

    added notes

--- solved/src/components/HelloDiv.js
+++ solved/src/components/HelloDiv.js
@@ -14,5 +14,19 @@ const HelloDiv = () => (
 		);
 
 
-
 export default HelloDiv;
+
+//need to impoert react
+
+// wrap everything in a parenthesis so that the arrow function understands that 
+//the following lines are to be rendered
+
+// lets talk about this arrow funstion => 
+// in our case we're goung to add () because we want to take advantage of implicit return 
+// within the arrow function 
+
+//in block body, we're goint to need to wrap everything in brackets and we're going to need a return statement 
+
+// start by creating a constant variable name
+//next make a return statement with a return statment  with a () because it will be multible lines 
+//and feplace name with consant variable with es6 syntax (name)
