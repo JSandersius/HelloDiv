f4f3e08a71c8c35e90e39360d7d45962f8bfc2f9
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-11 09:28:03 -0700

    edited hello div

--- solved/src/components/HelloDiv.js
+++ solved/src/components/HelloDiv.js
@@ -1,5 +1,18 @@
 import React from "react";
 
-const HelloDiv = () => null, 
+const HelloDiv = () => (
+	<div>
+		<h1> Hello my name is Julian </h1>
+		<h2> Things about be </h2> 
+		<ul>
+			<li>Three Brothers</li>
+			<li>Sublime or VS? </li>
+			<li> Medium to big dogs </li>
+		</ul>
+	</div>
+		
+		);
+
+
 
 export default HelloDiv;
