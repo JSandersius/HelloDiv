1cd126228e1f4a8c9390bac0df851475e9b1531e
Julian Sandersius <sandersiusjulian@gmail.com>
2018-06-11 19:53:47 -0700

    last commit of this file

--- solved/src/components/HelloDiv.js
+++ solved/src/components/HelloDiv.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-const HelloDiv = () => (
+const HelloDiv = () => {
+	const name = "Julian";
+	return(
 	<div>
-		<h1> Hello my name is Julian </h1>
+		<h1> Hello my name is {name}</h1>
 		<h2> Things about be </h2> 
 		<ul>
 			<li>Three Brothers</li>
@@ -12,6 +14,7 @@ const HelloDiv = () => (
 	</div>
 		
 		);
+}
 
 
 export default HelloDiv;
@@ -27,6 +30,12 @@ export default HelloDiv;
 
 //in block body, we're goint to need to wrap everything in brackets and we're going to need a return statement 
 
-// start by creating a constant variable name
-//next make a return statement with a return statment  with a () because it will be multible lines 
-//and feplace name with consant variable with es6 syntax (name)
+//start by creating a constant variable name
+//next make a return statement with a return statment  with a () because it will be multiple lines 
+//and replace name with consant variable with es6 syntax (name)
+//and when this is rendered it will have the same result as before
+//this is going to be helpful for large scale applications with greater understanding of the arrow function 
+
+
+//implicit return within the arrow function 
+
